93d7c17f1880e081fdee7827385eab0882a3fee0
Stephen Cripps <stephen.cripps@tutanota.com>
2024-11-12 14:58:12 +0000

    Initial Chat Setup

---
+++ SignalR/BlazorServer/Hubs/ChatHub.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorServer.Hubs;
+
+using Microsoft.AspNetCore.SignalR;
+
+public class ChatHub : Hub
+{
+    public Task SendMessage(string user, string message)
+    {
+        return Clients.All.SendAsync("ReceiveMessage", user, message);
+    }
+}
